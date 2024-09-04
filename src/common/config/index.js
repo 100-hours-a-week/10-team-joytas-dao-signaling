ad4da02b742ae07011b07b8d78ebce22d94f7951
jjikky <kok5417@skuniv.ac.kr>
2024-09-04 13:26:35 +0900

    chore: prod, dev 스프링 서버 주소 환경 분리

--- src/common/config/index.js
+++ src/common/config/index.js
@@ -18,9 +18,6 @@ const config = {
     // RTC
     maximumConnection: process.env.MAXIMUM_CONNECTION,
 
-    // SPRING
-    springServerUrl: process.env.SPRING_SERVER_URL,
-
     // REDIS
     redisPort: process.env.REDIS_PORT,
 };
@@ -28,11 +25,13 @@ const config = {
 switch (process.env.NODE_ENV) {
     case 'production':
         config.envMode = 'prod';
+        config.springServerUrl = process.env.SPRING_SERVER_URL_PROD;
         config.redisHost = process.env.REDIS_HOST_PROD;
         break;
 
     case 'development':
         config.envMode = 'dev';
+        config.springServerUrl = process.env.SPRING_SERVER_URL_DEV;
         config.redisHost = process.env.REDIS_HOST_DEV;
         break;
 
