50765ed61a8a8e21d78a636d47d8a3f34a330f18
jjikky <kok5417@skuniv.ac.kr>
2024-10-01 19:19:17 +0900

    chore : 패키지 구조 변경에 따른 import 변경 사항 #64

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const https = require('https');
 const config = require('./src/common/config');
 const expressLoader = require('./src/common/modules/express');
-const socketIoLoader = require('./src/socket.loader');
+const socketIoLoader = require('./src/handlers/socket.handler');
 const logger = require('./src/common/modules/logger');
 
 const app = express();
